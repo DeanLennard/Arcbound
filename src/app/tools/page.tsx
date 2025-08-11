95a8706909a9e3d5b1e243adc9bee75b128c0a47
Dean Lennard <dean@lawfullychaotic.co.uk>
2025-08-11 10:33:29 +0100

    update gambit form link

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -114,7 +114,7 @@ export default function ToolsPage() {
                 {/* Column 1 */}
                 <div className="space-y-4">
                     <ToolButton label="Admin Form" link="https://forms.gle/NmhGTzwKueQuQfaR6" />
-                    <ToolButton label="Phase Submission Form" link="https://forms.gle/ayPi4gDvykYMp8v86" />
+                    <ToolButton label="Phase Submission Form" link="https://forms.gle/AaqSZKDtFC9atraq5" />
                     <ToolButton label="Feedback & Complaints" link="https://forms.gle/gwcKLbFKe92dmPwf9" />
                 </div>
                 {/* Column 2 */}
